eb6b2c7e191032e3d2cf2392a832a36f144d930c
Mateus Holanda <mateus23ita@gmail.com>
2023-05-19 02:15:06 -0300

    Add fonts on mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text } from 'react-native'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+
+import blurBg from './src/assets/bg-blur.png'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
       <Text className="text-4xl font-bold text-gray-50">Hello!</Text>
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
