a8edb81c662d50b72383af19cc4d43ded1fddae6
Mateus Holanda <mateus23ita@gmail.com>
2023-05-19 21:16:58 -0300

    Bind a JWT to a user ID and verify it in the routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,9 @@
+import 'dotenv/config'
+
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const PORT = 3333
@@ -9,6 +13,12 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
