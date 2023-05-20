fd0a73f032470b33dd4c6e062a3f36ec1182d643
Mateus Holanda <mateus23ita@gmail.com>
2023-05-20 03:04:23 -0300

    Create middleware to check authentication and redirect user

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Logo from '../assets/logo.svg'
 
 export function Hero() {
@@ -16,12 +17,12 @@ export function Hero() {
         </p>
       </div>
 
-      <a
+      <Link
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-        href=""
+        href="/memories/new"
       >
         REGISTER MEMORY
-      </a>
+      </Link>
     </div>
   )
 }
