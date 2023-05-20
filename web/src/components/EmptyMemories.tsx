1ed828bb80685c695d15b6a6a56dad235e8dc370
Mateus Holanda <mateus23ita@gmail.com>
2023-05-20 02:30:43 -0300

    Create page New Memory

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -3,7 +3,7 @@ export function EmptyMemories() {
     <div className="flex flex-1 items-center justify-center">
       <p className="w-[320px] text-center leading-relaxed">
         You don&apos;t have any memories registered yet,{' '}
-        <a href="" className="underline hover:text-gray-50">
+        <a href="/memories/new" className="underline hover:text-gray-50">
           start your timeline now
         </a>
         !
